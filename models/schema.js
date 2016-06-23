ebd0752d11f87c0a8caab749aec60a3b01e16818
Matthew <mjauriemma@dons.usfca.edu>
2016-06-23 18:25:35 -0500

    Revert

--- models/schema.js
+++ models/schema.js
@@ -3,16 +3,16 @@ var sql = require('sql');
 exports.tweets = sql.define({
 	name: 'tweets',
 	columns: [
-		{ name: '`id`' },
-		{ name: '`month`' },
-		{ name: '`day`' },
-		{ name: '`year`' },
-		{ name: '`text`' },
-		{ name: '`location`' },
-		{ name: '`lat`' },
-		{ name: '`long`' },
-    { name: '`retweet_count`' },
-    { name: '`fav_count`' },
-    { name: '`date`' }
+		{ name: 'id' },
+		{ name: 'month' },
+		{ name: 'day' },
+		{ name: 'year' },
+		{ name: 'text' },
+		{ name: 'location' },
+		{ name: 'lat' },
+		{ name: 'long' },
+    { name: 'retweet_count' },
+    { name: 'fav_count' },
+    { name: 'date' }
 	]
 });
